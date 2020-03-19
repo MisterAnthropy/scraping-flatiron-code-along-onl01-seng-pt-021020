d700edb9284eabffd17cbc14afd1526c026c6fb8
Tyler Croley <tjcroley@outlook.com>
2020-03-19 22:42:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
   def get_page
-    
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry 
   end
   
   def print_courses
@@ -20,6 +22,6 @@ class Scraper
   end
   
 end
-
+Scraper.new.get_page
 
 
